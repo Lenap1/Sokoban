915aec2a3f12ff31af42e95390b2abf742a979fd
lenap <lenapfaffenbichler03@gmail.com>
2025-01-08 17:12:41 +0100

    New levels

--- server/server.test.js
+++ server/server.test.js
@@ -1,10 +1,10 @@
 import request from 'supertest';
-import server from './server';  // Importiere server.js
+import server from './server';  
 
 describe('API Tests', () => {
   let accessToken;
 
-  // Test für POST /api/login - sollte ein Token zurückgeben
+  
   it('POST /api/login - sollte ein Token zurückgeben', async () => {
     const response = await request(server)
       .post('/api/login')
@@ -16,21 +16,21 @@ describe('API Tests', () => {
 
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty('access_token');
-    accessToken = response.body.access_token;  // Speichern des Tokens für den nächsten Test
+    accessToken = response.body.access_token;  
   });
 
-  // Test für GET /api/data - sollte Daten zurückgeben
+ 
   it('GET /api/data - sollte eine Antwort mit Daten zurückgeben', async () => {
     const response = await request(server)
       .get('/api/data')
-      .set('Authorization', `Bearer ${accessToken}`);  // Setzt das Token in den Header
+      .set('Authorization', `Bearer ${accessToken}`);  
 
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty('data');
     expect(response.body.data).toBe('some data');
   });
 
-  // Test für GET /test - einfache Willkommensnachricht
+ 
   it('GET /test - sollte eine Willkommensnachricht zurückgeben', async () => {
     const response = await request(server).get('/test');
     expect(response.status).toBe(200);
